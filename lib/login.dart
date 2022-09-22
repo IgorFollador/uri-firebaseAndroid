6d4f0d879401e5ae3c1186457dbedbf83ddc4066
Igor Follador <igorledf@hotmail.com>
2022-09-21 22:31:01 -0300

    Implements Login with Firebase Auth Email and Password

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_android/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -11,8 +13,45 @@ class _LoginState extends State<Login> {
   TextEditingController _controllerEmail = TextEditingController();
   TextEditingController _controllerPassword = TextEditingController();
 
-  _validateFields() {}
-  _loginUser() {}
+  _validateFields() {
+    String email = _controllerEmail.text;
+    String password = _controllerPassword.text;
+
+    if (email.isNotEmpty && email.contains("@")) {
+      if (password.isNotEmpty && password.length >= 6) {
+        _loginUser(email, password);
+      }
+    }
+  }
+
+  _loginUser(email, password) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    auth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) => {
+              setState(() {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Home()));
+              }),
+            });
+  }
+
+  _validateLogin() {
+    User? userLogged = FirebaseAuth.instance.currentUser;
+    if (userLogged != null) {
+      setState(() {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Home()));
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    //TODO: implement initState
+    super.initState();
+    _validateLogin();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,8 +81,7 @@ class _LoginState extends State<Login> {
                     decoration: InputDecoration(hintText: "Password"),
                   ),
                 ),
-                ElevatedButton(
-                    onPressed: _validateFields, child: Text("Submmit"))
+                ElevatedButton(onPressed: _validateFields, child: Text("Login"))
               ],
             ),
           ),
